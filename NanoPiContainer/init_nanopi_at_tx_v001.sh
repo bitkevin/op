fe2604e431dc44b739452296279a112a2c8df306
hao-wang <z-hao-wang@users.noreply.github.com>
2022-04-09 16:09:26 -0500

    Update init_nanopi_at_tx_v001.sh

--- NanoPiContainer/init_nanopi_at_tx_v001.sh
+++ NanoPiContainer/init_nanopi_at_tx_v001.sh
@@ -51,65 +51,6 @@ sudo apt install -y inxi
 inxi -Fc0
 
 #
-# netplan settings
-#   WAN -- eth0
-#   LNA -- enp1s0
-#
-#
-#mkdir -p /etc/netplan
-#cat  << EOF >> /etc/netplan/config.yaml
-#network:
-#    version: 2
-#    renderer: networkd
-#    ethernets:
-#        eth0:
-#            dhcp4: yes
-#        enp1s0:
-#            addresses:
-#                - 192.168.1.8/24
-#EOF
-
-
-#
-# Enable NAT with iptables
-# !!! before 'apt install -y iptables-persistent' !!!
-#
-# Import iptables-persistent package configuration before package installation to automate the whole process.
-# https://sleeplessbeastie.eu/2018/09/10/how-to-make-iptables-configuration-persistent/
-#
-cat << EOF | sudo debconf-copydb pipe configdb --config=Name:pipe --config=Driver:Pipe
-Name: iptables-persistent/autosave_v4
-Template: iptables-persistent/autosave_v4
-Value: true
-Owners: iptables-persistent
-Flags: seen
-
-Name: iptables-persistent/autosave_v6
-Template: iptables-persistent/autosave_v6
-Value: true
-Owners: iptables-persistent
-Flags: seen
-EOF
-
-#
-# install iptables iptables-persistent
-#
-apt install -y iptables iptables-persistent
-mkdir -p /etc/iptables
-
-# add rule
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables-save > /etc/iptables/rules.v4
-
-
-#
-# enable net.ipv4.ip_forward = 1
-#
-sed -i 's/^#net\.ipv4\.ip_forward=1/net\.ipv4\.ip_forward=1/g' /etc/sysctl.conf
-# apply
-sysctl -p
-
-#
 # lock user 'pi'
 #
 # disable shell for 'pi'
