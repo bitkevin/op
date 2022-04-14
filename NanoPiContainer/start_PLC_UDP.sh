71b3be61419e92d8a55a58ae8f23cd9a985218c8
hao-wang <z-hao-wang@users.noreply.github.com>
2022-04-14 08:59:27 -0500

    Update start_PLC_UDP.sh

--- NanoPiContainer/start_PLC_UDP.sh
+++ NanoPiContainer/start_PLC_UDP.sh
@@ -2,7 +2,7 @@ wget -P ~/src --recursive --no-parent http://192.168.15.200:8080/plc-monitor
 mv src/192.168.15.200\:8080/plc-monitor ~/
 rm -rf ~/src
 cd ~/plc-monitor && npm ci
-pm2 start src/listen.js
+pm2 start src/listenPLC.js
 
 pm2 save
 pm2 startup
