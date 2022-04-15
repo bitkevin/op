83d49904388603d593f5c50204d8164c43758346
hao-wang <z-hao-wang@users.noreply.github.com>
2022-04-15 18:32:20 -0500

    Create reset_ip.sh

---
+++ NanoPiContainer/reset_ip.sh
@@ -0,0 +1,11 @@
+cat MM EOF > /etc/netplan/01-network-manager-all.yaml
+network:
+    version: 2
+     renderer: networkd
+     ethernets:
+         enp1s0:
+             dhcp4: true
+         ens1:
+             dhcp4: true
+EOF
+netplan apply
