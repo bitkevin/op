dd30af75e007f54769cced003e6611b8334fbda0
hao-wang <z-hao-wang@users.noreply.github.com>
2022-04-15 22:44:04 -0500

    Update init_nanopi_at_tx_v001.sh

--- NanoPiContainer/init_nanopi_at_tx_v001.sh
+++ NanoPiContainer/init_nanopi_at_tx_v001.sh
@@ -80,11 +80,8 @@ mkdir ~/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCxWtdRN3XvjgZ0HpLLDHjNNa8H2EEpb1QADNPFyqs6jijIgIdRj8fzGRHfcn2/dIJeQIZm9BsSUzLZivdV8Wt7g8h7YT/4M5AxMuMAMJZs6qFWrrdNRk0R8FZtBDQ2eRHe5qwllurwgWQ92zWvjG+/Hx3odfbM8oPC1f27exmGcX4q5dZCdjqpNxFX3QdrGYcIRFzu5CdyzeIt7HTK5WlMYR06wwsKZijR39BbFXCUSAl2ZYeBnpMPrs61xFy0Ji89m5z0EAkvstIjq2SGtO6jJNsxzAcTzdC66KqQpKPK/FVpXF2+LFf8YgsZkEhMfYL8rQeIFUGStkEllIyHpoa3CIBYqcqrduco0BhsVlitO00i4jMz9H6zu4ub3W4KKh5xHZRqb2KdellImUvbp1rPvT9TOKp6rOJXBPjcULhVg6DutU6yPnKWRS2oPUBiJVmVy5qNsebjRLRa0awAMdkRhBP3Jxs1hlGtlETtmtbdPMP1cXSl84vRuhb5h/f8gMc= proxy1@proxy-main1" >> ~/.ssh/authorized_keys
 chmod 700 ~/.ssh/authorized_keys
 
-echo "enter new root password"
-passwd
+
 
 
 touch /root/init.done
-
-# reboot system
-# reboot
+echo "please change new root password using passwd"
