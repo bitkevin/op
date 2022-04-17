11e3defdecc4b2a966b17d4b2bd75ac95cb05b1f
hao-wang <z-hao-wang@users.noreply.github.com>
2022-04-16 22:25:08 -0500

    Create auto_config_ip_no_plc.sh

---
+++ NanoPiContainer/auto_config_ip_no_plc.sh
@@ -0,0 +1,40 @@
+LAN1=enp1s0
+LAN2=ens1
+
+IP=`ifconfig $LAN1 | grep 'inet' | cut -d: -f2 | awk '{print $2}'`
+echo $IP
+
+IPStart=`echo $IP | cut -c 1-4`
+echo "IPStart=${IPStart}"
+
+IP3rd=`echo $IP | cut -f3 -d"."`
+echo "IP3rd=${IP3rd}"
+
+if [ "$IPStart" = "10.1" ]; then
+    echo "found dhcp in 10.1 range"
+else
+	echo "ip $IP is not in expected range"
+	exit
+fi;
+
+NewIP="10.2.${IP3rd}.253"
+NewIPDynamic="10.1.${IP3rd}.253"
+
+echo "newIP=${NewIP}"
+
+cat  << EOF > /etc/netplan/01-network-manager-all.yaml
+network:
+    version: 2
+    renderer: networkd
+    ethernets:
+        ${LAN1}:
+            addresses:
+              - ${NewIP}/24
+              - ${NewIPDynamic}/24
+            gateway4: "10.2.${IP3rd}.254"
+            nameservers:
+              addresses: [8.8.8.8, 1.1.1.1]
+        ${LAN2}:
+            dhcp4: true
+EOF
+netplan apply
